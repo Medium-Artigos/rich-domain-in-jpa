7b0257a84b9c699106ae161babc200d46128ac20
lfernandes <lfernandes.iteris@alelo.com.br>
2024-12-09 12:23:42 -0300

    aditiion logback.xml

---
+++ src/main/java/br/com/ldf/application/aspect/annotations/Masked.java
@@ -0,0 +1,12 @@
+package br.com.ldf.application.aspect.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Masked {
+    String pattern() default "*********";
+}
